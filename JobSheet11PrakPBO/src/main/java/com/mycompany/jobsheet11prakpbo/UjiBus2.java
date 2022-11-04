578a5e22da2c0182b11dcdbd15c7af6eb076099b
21343062 <112645631+21343062@users.noreply.github.com>
2022-11-04 16:27:17 +0700

    Add files via upload
    Nama : Nesa Athallah Ramde Pikuang
    NIM : 21343062

---
+++ JobSheet11PrakPBO/src/main/java/com/mycompany/jobsheet11prakpbo/UjiBus2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jobsheet11prakpbo;
+
+/**
+ * Created by 21343062-Nesa Athallah
+ * @author user
+ */
+public class UjiBus2 {
+    public static void main(String args[]){
+        Bus2 busBesar =  new Bus2(40);
+        busBesar.cetak();
+        
+        busBesar.addPenumpang(15);
+        busBesar.cetak();
+        
+        busBesar.addPenumpang(5);
+        busBesar.cetak();
+        
+        busBesar.addPenumpang(26);
+        busBesar.cetak();
+    }
+}
